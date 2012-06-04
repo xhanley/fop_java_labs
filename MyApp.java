4e9308b9e5f97cf9cd067a5d7e8ad94568f1227b
Donnchadh O h-Ainle <xhanley@gmail.com>
2012-06-04 15:04:32 +0100

    This is the first commit of a cloned repository from local mac using terminal.

---
+++ MyApp.java
@@ -0,0 +1,31 @@
+class MyApp
+{
+	public static void main(String[] args)
+	{
+		int a = 99;
+		System.out.println("Hello!");
+		myFunc();
+		AnotherClass myObj = new AnotherClass();
+		myObj.action();
+	}
+
+	public static void myFunc()
+	{
+		System.out.println("MyFunction!");
+	}
+}
+
+class AnotherClass
+{
+  int b = 3;
+
+  public AnotherClass()
+  {
+    b++;
+  }
+
+  void action()
+  {
+    System.out.println(b);
+  }
+}
